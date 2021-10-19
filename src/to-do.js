8627a0810653f97c0640274143301f8893c38e47
ktarotaro <88434336+ktarotaro@users.noreply.github.com>
2021-10-19 18:59:19 +0900

    一時保存

--- src/to-do.js
+++ src/to-do.js
@@ -5,7 +5,21 @@ const onClickAdd = () => {
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
 
-  alert(inputText);
+  //li生成
+  const li = document.createElement("li");
+
+  //div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+  div.innerText = inputText;
+  // alert(inputText);
+
+  //liタグの子要素に各要素を設定
+  li.appendChild(div);
+  console.log(li);
+
+  //未完了のリストに追加
+  document.getElementById("incomlete-list").appendChild(li);
 };
 
 document
