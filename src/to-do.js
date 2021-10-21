000f999e27c6482a879998db2717a15a40673116
ktarotaro <88434336+ktarotaro@users.noreply.github.com>
2021-10-21 18:50:31 +0900

    保存

--- src/to-do.js
+++ src/to-do.js
@@ -11,17 +11,36 @@ const onClickAdd = () => {
   //div生成
   const div = document.createElement("div");
   div.className = "list-row";
-  div.innerText = inputText;
+  // div.innerText = inputText;
   // alert(inputText);
+
+  //pタグ生成
+  const paragraph = document.createElement("p");
+  paragraph.innerText = inputText;
 
   //button（完了）タグ生成
   const completeButton = document.createElement("button");
   completeButton.innerText = "完了";
-  console.log(completeButton);
+  completeButton.addEventListener("click", () => {
+    alert("完了");
+  });
+
+  //button（削除）タグ生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    alert("削除");
+  });
 
   //liタグの子要素に各要素を設定
   li.appendChild(div);
-  // console.log(li);
+
+  //divタグの子要素に各要素を設定
+  div.appendChild(paragraph);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+
+  console.log(li);
 
   //未完了のリストに追加
   document.getElementById("incomlete-list").appendChild(li);
